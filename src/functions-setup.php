6b92020bef342febd4c911010c37f7835f69c560
benlumia007 <benlumia007@users.noreply.github.com>
2021-10-23 09:56:33 -0700

    delete src/Filter/functions-filters.php and reconfigured the src/functions-filters.php

--- src/functions-setup.php
+++ src/functions-setup.php
@@ -52,7 +52,6 @@ function comments_template( $template ) {
  * Add excerpt more
  */
 function excerpt_more() {
-	global $post;
 	$more = ' ...';
 
 	return esc_html( $more );
@@ -67,41 +66,17 @@ function archive_title() {
 	} elseif ( is_tag() ) {
 		$title = single_tag_title( '', false );
 	} elseif ( is_author() ) {
-		$title = esc_html__( 'Author', 'ecclesiastical' ) . '<span class="archive-description">' . get_the_author() . '</span>';
+		$title = get_the_author();
 	} elseif ( is_year() ) {
-		$title = esc_html__( 'Year', 'ecclesiastical' ) . '<span class="archive-description">' . get_the_date( _x( 'Y', 'yearly archives date format', 'ecclesiastical' ) ) . '</span>';
+		$title = get_the_date( _x( 'Y', 'yearly archives date format', 'backdrop' ) );
 	} elseif ( is_month() ) {
-		$title = esc_html__( 'Month', 'ecclesiastical' ) . '<span class="archive-description">' . get_the_date( _x( 'F', 'monthly archives date format', 'ecclesiastical' ) ) . '</span>';
+		$title =get_the_date( _x( 'F', 'monthly archives date format', 'backdrop' ) );
 	} elseif ( is_day() ) {
-		$title = esc_html__( 'Day', 'ecclesiastical' ) . '<span class="archive-description">' . get_the_date( _x( 'F j Y', 'daily archives date format', 'ecclesiastical' ) ) . '</span>';
-	} elseif ( is_tax( 'post_format' ) ) {
-		if ( is_tax( 'post_format', 'post-format-aside' ) ) {
-			$title = _x( 'Asides', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-			$title = _x( 'Galleries', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-			$title = _x( 'Images', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-			$title = _x( 'Videos', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-			$title = _x( 'Quotes', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-			$title = _x( 'Links', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-			$title = _x( 'Statuses', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-			$title = _x( 'Audio', 'post format archive title', 'ecclesiastical' );
-		} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-			$title = _x( 'Chats', 'post format archive title', 'ecclesiastical' );
-		}
+		$title = get_the_date( _x( 'F j Y', 'daily archives date format', 'backdrop' ) );
 	} elseif ( is_post_type_archive() ) {
-		$title = esc_html__( 'Archives', 'ecclesiastical' ) . '<span class="archive-description">' . post_type_archive_title( '', false ) . '</span>';
+		$title = post_type_archive_title( '', false );
 	} elseif ( is_tax() ) {
-		$tax = get_taxonomy( get_queried_object()->taxonomy );
-		// Translators: 1 = singular name, 2 = single term title.
-		$title = sprintf( __( '%1$s: %2$s', 'ecclesiastical' ), $tax->labels->singular_name, single_term_title( '', false ) );
-	} else {
-		$title = esc_html__( 'Archives', 'ecclesiastical' );
+		$title = single_term_title( '', false );
 	}
 	return $title;
 }
