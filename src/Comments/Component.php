d1b89a6b00d351c6b508a344c7aecf1902470df4
benlumia007 <benlumia007@users.noreply.github.com>
2021-10-26 23:17:05 -0700

    Comments setup

---
+++ src/Comments/Component.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Backdrop Core ( src/Component/Customize.php )
+ *
+ * @package   Backdrop Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Theme\Comments;
+
+class Component {
+    public static function display_comments_list() {
+        echo "shit";
+    }
+}
