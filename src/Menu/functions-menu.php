2354641fc91d6f89f6a7c87bae3691c69e5ce8bf
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-28 22:26:00 -0700

    fixed primary menu

--- src/Menu/functions-menu.php
+++ src/Menu/functions-menu.php
@@ -28,14 +28,16 @@ function display( $type, $items = [] ) {
 function menu( $item ) {
 	if ( 'primary' === $item ) {
 		if ( has_nav_menu( 'primary' ) ) { ?>
-			<nav id="primary" class="primary-navigation">
+			<nav id="primary" class="primary-menu">
 				<button class="menu-toggle" aria-conrol="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'backdrop' ); ?></button>
 				<?php
 					wp_nav_menu(
 						array(
 							'theme_location' => 'primary',
+							'container'      => '',
 							'menu_id'        => 'primary-menu',
-							'menu_class'     => 'primary-menu nav-menu',
+							'menu_class'     => 'menu-items',
+							'depth'          => 3
 						)
 					);
 				?>
