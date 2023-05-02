5aa982ea2592226653792bb63e0b8b8bc8e52124
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-02 14:30:06 -0700

    updated

--- src/functions-filters.php
+++ src/functions-filters.php
@@ -15,19 +15,19 @@
 namespace Backdrop\Theme;
 
 # Filters the WordPress element classes.
-add_filter( 'body_class',    __NAMESPACE__ . '\body_class_filter',    ~PHP_INT_MAX, 2 );
+// add_filter( 'body_class',    __NAMESPACE__ . '\body_class_filter',    ~PHP_INT_MAX, 2 );
 
 # Add extra support for post types.
-add_action( 'init', __NAMESPACE__ . '\post_type_support', 15 );
+// add_action( 'init', __NAMESPACE__ . '\post_type_support', 15 );
 
 # Default excerpt more.
-add_filter( 'excerpt_more', __NAMESPACE__ . '\excerpt_more', 5 );
+// add_filter( 'excerpt_more', __NAMESPACE__ . '\excerpt_more', 5 );
 
 # Adds common theme items to <head>.
-add_action( 'wp_head', __NAMESPACE__ . '\meta_charset',   0 );
-add_action( 'wp_head', __NAMESPACE__ . '\meta_viewport',  1 );
-add_action( 'wp_head', __NAMESPACE__ . '\meta_generator', 1 );
-add_action( 'wp_head', __NAMESPACE__ . '\link_pingback',  3 );
+// add_action( 'wp_head', __NAMESPACE__ . '\meta_charset',   0 );
+// add_action( 'wp_head', __NAMESPACE__ . '\meta_viewport',  1 );
+// add_action( 'wp_head', __NAMESPACE__ . '\meta_generator', 1 );
+// add_action( 'wp_head', __NAMESPACE__ . '\link_pingback',  3 );
 
 # Filter the comments template
-add_filter( 'comments_template', __NAMESPACE__ . '\comments_template' );
+// add_filter( 'comments_template', __NAMESPACE__ . '\comments_template' );
