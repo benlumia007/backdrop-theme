4239c37a0277d564554d1c6854db487ce3db93bf
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-02 14:27:50 -0700

    updated

---
+++ src/Provider.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Backdrop
+ *
+ * @package   Backdrop
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2019-2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/backdrop-theme
+ */
+
+namespace Backdrop\Theme;
+use Backdrop\Core\ServiceProvider;
+
+/**
+ * Theme provider.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Provider extends ServiceProvider {
+
+	/**
+	 * Bootstrap action/filter hook calls.
+	 *
+	 * @since  1.0.0
+	 * @access protected
+	 * @return void
+	 */
+	public function boot() {
+		require_once 'bootstrap-filters.php';
+	}
+}
