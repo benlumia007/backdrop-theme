24a0ae1de00efb3202c91b2c03e7963240f23298
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-29 15:14:51 -0700

    updated

--- src/Comment/functions-comment.php
+++ src/Comment/functions-comment.php
@@ -11,7 +11,7 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-namespace Hybrid\Theme\Comment;
+namespace Backdrop\Theme\Comment;
 
 /**
  * Returns a hierarchy for the current comment.
