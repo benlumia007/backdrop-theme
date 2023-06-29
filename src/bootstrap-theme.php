598b842ca7f5722500de6612f873fb6c3b16481e
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-29 15:11:43 -0700

    updated

---
+++ src/bootstrap-theme.php
@@ -0,0 +1,7 @@
+<?php 
+
+
+require_once 'src/Entry/functions-entry.php';
+require_once 'src/Site/functions-site.php';
+require_once 'src/functions-filters.php';
+require_once 'src/functions-helpers.php';
