735d396cfef85db639b830468c1b3ab89c0ea841
Benjamin Lu <benlumia007@users.noreply.github.com>
2024-02-29 09:31:57 -0800

    add a function for mod

--- src/functions-helpers.php
+++ src/functions-helpers.php
@@ -11,6 +11,24 @@
 
 namespace Backdrop\Theme;
 
+/**
+ * This is a wrapper function for core WP's `get_theme_mod()` function.  Core
+ * doesn't provide a filter hook for the default value (useful for child themes).
+ * The purpose of this function is to provide that additional filter hook.  To
+ * filter the final theme mod, use the core `theme_mod_{$name}` filter hook.
+ *
+ * @param  string $name
+ * @param  mixed  $default
+ * @return mixed
+ */
+function mod( $name, $default = false ) {
+    return get_theme_mod(
+        $name,
+        apply_filters( "backdrop/theme/mod/{$name}/default", $default )
+    );
+}
+
+
 function is_classicpress() {
     if ( function_exists( 'classicpress_version' ) ) {
         return true;
